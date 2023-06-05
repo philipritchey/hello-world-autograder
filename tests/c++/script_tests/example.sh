5239aeb55e744a0c6c624e02cdf899931488f330
Philip Ritchey <philipritchey@users.noreply.github.com>
2023-06-05 00:46:34 -0500

    Initial commit

---
+++ tests/c++/script_tests/example.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+# an example script (custom) test
+
+usage() {
+  echo "Usage: $0 [-h]"
+  echo "  -h show this help message and exit"
+}
+
+debugmode=0
+while getopts "dht:" flag; do
+  case "${flag}" in
+    h | *) # display help
+      usage >> DEBUG
+      echo 100 > OUTPUT
+      echo "PASS: correct behavior" >> DEBUG
+      exit 0
+      ;;
+  esac
+done
+
+if g++ -std=c++17 -pedantic-errors -g code.cpp code_interactive.cpp > DEBUG 2>&1; then
+    echo "X" | ./a.out > OUT
+    if grep -qi "invalid input" OUT; then
+        echo 100 > OUTPUT
+        echo "PASS: correct behavior" >> DEBUG
+    else
+        echo 0 > OUTPUT
+        echo "FAIL: incorrect behavior" >> DEBUG
+    fi
+else
+    echo 0 > OUTPUT
+    echo "FAIL: compilation errors" >> DEBUG
+fi
